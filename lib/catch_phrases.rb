a2e4d2ae418cabf498704bd5f789368265207a30
Ally Morson <ally.morson@gmail.com>
2019-09-02 22:44:06 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,8 @@
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
-end
-
+  puts s-a me, Mario!"
+endphrase "It'
+phrase
 def toadstool
   puts status
 end
